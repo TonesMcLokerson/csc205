239b8088fe69a6c183d2d2388b51c05f99a94b09
Tony Demyan <anthonydemyan88@gmail.com>
2018-01-22 11:41:01 -0700

    HelloWorld

---
+++ java/csc205/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * This Java application prints the phrase "hello, world"
+ * to the standard output stream.
+ *
+ * @creator Tony Demyan
+ * @generated 02018.01.20
+ */
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.print("\"hello, world\" from Tony Demyan\n");
+  }
+} 
