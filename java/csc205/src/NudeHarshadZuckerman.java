d11b760a5bb57ee6d29cd965c51b9cff5f5af449
Tony Demyan <anthonydemyan88@gmail.com>
2018-01-29 11:12:58 -0700

    Second Commit -- Corrected spacing.

--- java/csc205/src/NudeHarshadZuckerman.java
+++ java/csc205/src/NudeHarshadZuckerman.java
@@ -13,66 +13,77 @@
  */
 
 public class NudeHarshadZuckerman {
-	public static void main(String[] args) {
-		final int NUMBERS_PER_LINE = 8;
-		int i = 0;
-		displayTitle();
-		for (int x = 1; x <= Short.MAX_VALUE; x++) {
-			boolean nude = calcNude(x);
-			boolean harshad = calcHarshad(x);
-			boolean zuckerman = calcZuckerman(x);
+	
+  public static void main(String[] args) {
+	  
+	  final int NUMBERS_PER_LINE = 8;
+	  int i = 0;
+	  displayTitle();
+	  
+	  for (int x = 1; x <= Short.MAX_VALUE; x++) {
+	    boolean nude = calcNude(x);
+	    boolean harshad = calcHarshad(x);
+		boolean zuckerman = calcZuckerman(x);
 
-			if (nude && harshad && zuckerman) {
-				System.out.print(x + ",");
-				i++;
+		if (nude && harshad && zuckerman) {
+		  System.out.print(x + ",");
+	        i++;
 
-				if (i % NUMBERS_PER_LINE == 0) {
-					System.out.print("\n");
-					i = 0;
-				}
-			}
+		  if (i % NUMBERS_PER_LINE == 0) {
+		    System.out.print("\n");
+			i = 0;
+		  }
 		}
+	   }
 	}
 
 	public static boolean calcNude(int x) {
-		int digit = 0, dummy, number;
-		boolean answer = false;
-		number = x;
-		dummy = number;
-		outerloop: while (number > 0) {
-			digit = number % 10;
-			number = number / 10;
-			if (digit == 0) {
-				answer = false;
-				number = -1;
-			}
-			while (digit != 0) {
-				if (dummy % digit != 0) {
-					answer = false;
-					number = -1;
-					break outerloop;
-				} else {
-					answer = true;
-					break outerloop;
-				}
-			}
+		
+	  int digit = 0, dummy, number;
+	  boolean answer = false;
+	  number = x;
+	  dummy = number;
+	  
+	  outerloop: 
+	  while (number > 0) {
+	    digit = number % 10;
+		number = number / 10;
+		
+		if (digit == 0) {
+		  answer = false;
+		  number = -1;
 		}
+		
+		while (digit != 0) {
+		  if (dummy % digit != 0) {
+		    answer = false;
+			number = -1;
+			break outerloop;
+		  } else {
+		    answer = true;
+			break outerloop;
+			}
+		  }
+	   }
 		return answer;
-	}
+	 }
 
 	public static boolean calcHarshad(int x) {
+		
 		int number, dummy, digit = 0, sum = 0;
 		boolean answer = false;
 		number = x;
 		dummy = number;
 
 		{
-			do {
-				digit = number % 10;
-				sum = sum + digit;
-				number = number / 10;
-			} while (number > 0);
+		do {
+		  digit = number % 10;
+		  sum = sum + digit;
+		  number = number / 10;
+		  
+		} while (number > 0);
 		}
+		
 		int check = dummy % sum;
 		if (check == 0) {
 			answer = true;
@@ -81,32 +92,37 @@ public class NudeHarshadZuckerman {
 	}
 
 	public static boolean calcZuckerman(int x) {
-		int number, dummy, remainder = 0, pro = 1, ten = 10;
-		boolean answer = false;
-		number = x;
-		dummy = number;
+		
+	  int number, dummy, remainder = 0, pro = 1, ten = 10;
+	  boolean answer = false;
+	  number = x;
+	  dummy = number;
 
-		while (number > 0) {
-			pro *= (number % ten); //find digit using u=u%10
-			number /= ten;
+	  while (number > 0) {
+	    pro *= (number % ten); //find digit using u=u%10
+		number /= ten;
+	  }
+	  if (pro != 0) {
+		remainder = dummy % pro;
+	  } else {
+		return answer;
 		}
-		if (pro != 0) {
-			remainder = dummy % pro;
-		} else {
-			return answer;
-		}
-		if (remainder == 0) {
+	  
+	  if (remainder == 0) {
 			answer = true;
 		}
-		return answer;
+	  return answer;
 	}
 
 	public static void displayTitle() {
+		
 		String de = "Nude";
 		String ad = "Harshad";
 		String an = "Zuckerman";
 		String title = (de + " " + ad + " and " + an + " Numbers\n");
+		
 		System.out.print(title);
+		
 		for (int i = 2, len = title.length(); i <= len; i++) {
 			System.out.print("=");
 		}
